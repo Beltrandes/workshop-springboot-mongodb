8eb582a648559d24933ad340cb9a2a4b90bd3d05
Beltrandes Marques <beltsmarques@hotmail.com>
2024-01-03 12:27:29 -0300

    Database instantiation

---
+++ src/main/java/com/beltrandes/springboot_mongodb/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.beltrandes.springboot_mongodb.config;
+
+import com.beltrandes.springboot_mongodb.domain.User;
+import com.beltrandes.springboot_mongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        var maria = new User(null, "Maria Brown", "maria@gmail.com");
+        var alex = new User(null, "Alex Green", "alex@gmail.com");
+        var bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+    }
+}
